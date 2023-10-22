0429a67fcb74bc022d1a5508533b89271eca1c43
Neha C <73354497+nehachangela@users.noreply.github.com>
2023-10-22 10:44:21 -0400

    Add files via upload

--- logicProject3.js
+++ logicProject3.js
@@ -1,20 +1,35 @@
-console.log("show data:",data)
+const url = "https://127.0.0.1:5000/api/v1.0/NewYorkCity"
 
-let dates = data.map(function (row){
-    return row.date
+d3.json(url).then(function(data){
+    let dates = data.map(function (row){
+        return row.date
+    });
+    console.log("Dates:",dates);
+    
+    let lineData = {
+        type: "line",
+        x: dates,
+        y: data.map(row => row.nyc_max_temp),
+    };
+              
+    let lineLayout = {
+        title: "Temperature",
+        xaxis: { title: "Date" }
+    };
+    Plotly.newPlot("plot", [lineData], lineLayout);
+
+    let barData = {
+        type: "bar",
+        x: dates,
+        y: data.map(row => row.nyc_prcp)
+    };
+    let barLayout = {
+        title: "Precipition",
+        xaxis: { title: "Date" }
+    };
+
+    Plotly.newPlot("plot", [barData], barLayout);
 });
-console.log("Dates:",dates);
-
-let barData = {
-    type: "bar",
-    x: dates,
-    y: data.map(row => row.nyc_prcp)
-};
-let barLayout = {
-    title: "Precipition",
-    xaxis: { title: "Date" }
-};
-Plotly.newPlot("plot", [barData], barLayout);
 /*
 let lineData = {
     type: "line",
