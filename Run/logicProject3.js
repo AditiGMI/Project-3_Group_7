9d6c276af187a7cee577d5547eace7b548e09957
Neha C <73354497+nehachangela@users.noreply.github.com>
2023-10-23 15:34:43 -0400

    Add files via upload

--- Run/logicProject3.js
+++ Run/logicProject3.js
@@ -1,9 +1,254 @@
-let nycdata = 'nycdata.json';
-let beijingdata = 'bjgdata.json';
-let londondata = 'lnddata.json';
-let tokyodata = 'tkydata.json';
-let mexicocitydata = 'mxcdata.json';
 
+// Define the URLs to your JSON data for each city
+const cityData = {
+  NewYorkCity: {
+    url: 'https://raw.githubusercontent.com/AditiGMI/Project-3_Group_7/main/jsondata/nycdata.json',
+    prcpKey: 'nyc_prcp',
+    tempKey: 'nyc_max_temp',
+  },
+  Beijing: {
+    url: 'https://raw.githubusercontent.com/AditiGMI/Project-3_Group_7/main/jsondata/bjgdata.json',
+    prcpKey: 'bjg_prcp',
+    tempKey: 'bjg_max_temp',
+  },
+  London: {
+    url: 'https://raw.githubusercontent.com/AditiGMI/Project-3_Group_7/main/jsondata/lnddata.json',
+    prcpKey: 'lnd_prcp',
+    tempKey: 'lnd_max_temp',
+  },
+  Tokyo: {
+    url: 'https://raw.githubusercontent.com/AditiGMI/Project-3_Group_7/main/jsondata/tkydata.json',
+    prcpKey: 'tky_prcp',
+    tempKey: 'tky_max_temp',
+  },
+  MexicoCity: {
+    url: 'https://raw.githubusercontent.com/AditiGMI/Project-3_Group_7/main/jsondata/mxcdata.json',
+    prcpKey: 'mxc_prcp',
+    tempKey: 'mxc_max_temp',
+  },
+};
+
+// Function to fetch JSON data and create charts
+async function fetchDataAndCreateCharts(selectedCity) {
+  try {
+    // Fetch the JSON data for the selected city
+    const cityInfo = cityData[selectedCity];
+    const response = await fetch(cityInfo.url);
+    const data = await response.json();
+
+    // Create the bar chart for precipitation
+    const dates = data.map(item => item.date);
+    const precipitation = data.map(item => parseFloat(item[cityInfo.prcpKey]));
+    const barData = {
+      type: "bar",
+      x: dates,
+      y: precipitation,
+    };
+    const barLayout = {
+      title: "Precipitation",
+      xaxis: { title: "Date" },
+      yaxis: { title: "Precipitation (mm)" },
+    };
+    Plotly.newPlot("barChart", [barData], barLayout);
+
+    // Create the line chart for maximum temperature
+    const maxTemperatures = data.map(item => parseFloat(item[cityInfo.tempKey]));
+    const lineData = {
+      type: "line",
+      x: dates,
+      y: maxTemperatures,
+    };
+    const lineLayout = {
+      title: "Maximum Temperature",
+      xaxis: { title: "Date" },
+      yaxis: { title: "Temperature (°C)" },
+    };
+    Plotly.newPlot("lineChart", [lineData], lineLayout);
+
+    // Display metadata
+    displayMetadata(data.metadata);
+  } catch (error) {
+    console.error("Error fetching or processing data:", error);
+  }
+}
+
+// Function to handle dropdown change
+function onCityChange() {
+  const selectedCity = document.getElementById("cityDropdown").value;
+  fetchDataAndCreateCharts(selectedCity);
+}
+
+// Event listener for dropdown change
+document.getElementById("cityDropdown").addEventListener("change", onCityChange);
+
+// Initialize the dashboard with the first city (e.g., New York City)
+onCityChange();
+
+
+
+/*
+// Define the URLs to your JSON data
+const nycdata = 'https://raw.githubusercontent.com/AditiGMI/Project-3_Group_7/main/jsondata/nycdata.json';
+const beijingdata = 'https://raw.githubusercontent.com/AditiGMI/Project-3_Group_7/main/jsondata/bjgdata.json';
+const londondata = 'https://raw.githubusercontent.com/AditiGMI/Project-3_Group_7/main/jsondata/lnddata.json';
+const tokyodata = 'https://raw.githubusercontent.com/AditiGMI/Project-3_Group_7/main/jsondata/tkydata.json';
+const mexicocitydata = 'https://raw.githubusercontent.com/AditiGMI/Project-3_Group_7/main/jsondata/mxcdata.json';
+
+// Define a mapping of city names to data URLs
+const dataUrls = {
+  NewYorkCity: nycdata,
+  Beijing: beijingdata,
+  London: londondata,
+  Tokyo: tokyodata,
+  MexicoCity: mexicocitydata
+};
+
+// Function to fetch JSON data and create charts
+async function fetchDataAndCreateCharts(selectedCity) {
+  try {
+    // Fetch the JSON data for the selected city
+    const response = await fetch(dataUrls[selectedCity]);
+    const data = await response.json();
+
+    // Create the bar chart for precipitation
+    const dates = data.map(item => item.date);
+    const precipitation = data.map(item => parseFloat(item.nyc_prcp));
+    const barData = {
+      type: "bar",
+      x: dates,
+      y: precipitation,
+    };
+    const barLayout = {
+      title: "Precipitation",
+      xaxis: { title: "Date" },
+      yaxis: { title: "Precipitation (mm)" },
+    };
+    Plotly.newPlot("barChart", [barData], barLayout);
+
+    // Create the line chart for maximum temperature
+    const maxTemperatures = data.map(item => parseFloat(item.nyc_max_temp));
+    const lineData = {
+      type: "line",
+      x: dates,
+      y: maxTemperatures,
+    };
+    const lineLayout = {
+      title: "Maximum Temperature",
+      xaxis: { title: "Date" },
+      yaxis: { title: "Temperature (°C)" },
+    };
+    Plotly.newPlot("lineChart", [lineData], lineLayout);
+
+    // Display metadata
+    displayMetadata(data.metadata);
+  } catch (error) {
+    console.error("Error fetching or processing data:", error);
+  }
+}
+
+// Function to handle dropdown change
+function onCityChange() {
+  const selectedCity = document.getElementById("cityDropdown").value;
+  fetchDataAndCreateCharts(selectedCity);
+}
+
+// Event listener for dropdown change
+document.getElementById("cityDropdown").addEventListener("change", onCityChange);
+
+// Initialize the dashboard with the first city (e.g., New York City)
+onCityChange();
+
+
+
+
+/*
+let nycdata = 'https://github.com/AditiGMI/Project-3_Group_7/blob/main/jsondata/nycdata.json';
+let beijingdata = ' https://github.com/AditiGMI/Project-3_Group_7/blob/main/jsondata/bjgdata.json';
+let londondata = 'https://github.com/AditiGMI/Project-3_Group_7/blob/main/jsondata/lnddata.json';
+let tokyodata = 'https://github.com/AditiGMI/Project-3_Group_7/blob/main/jsondata/tkydata.json';
+let mexicocitydata = 'https://github.com/AditiGMI/Project-3_Group_7/blob/main/jsondata/mxcdata.json';
+
+// Your JSON data
+const cityData = {
+  NewYorkCity: [nycdata],
+  Beijing: [beijingdata],
+  London: [londondata],
+  Tokyo: [tokyodata],
+  MexicoCity: [mexicocitydata]
+};
+
+// Function to create a bar chart for precipitation
+function createPrecipitationBarChart(data) {
+  const dates = data.map(item => item.date);
+  const precipitation = data.map(item => parseFloat(item.nyc_prcp));
+
+  const barData = {
+      type: "bar",
+      x: dates,
+      y: precipitation,
+  };
+
+  const barLayout = {
+      title: "Precipitation",
+      xaxis: { title: "Date" },
+      yaxis: { title: "Precipitation (mm)" },
+  };
+
+  Plotly.newPlot("barChart", [barData], barLayout);
+}
+
+// Function to create a line chart for maximum temperature
+function createTemperatureLineChart(data) {
+  const dates = data.map(item => item.date);
+  const maxTemperatures = data.map(item => parseFloat(item.nyc_max_temp));
+
+  const lineData = {
+      type: "line",
+      x: dates,
+      y: maxTemperatures,
+  };
+
+  const lineLayout = {
+      title: "Maximum Temperature",
+      xaxis: { title: "Date" },
+      yaxis: { title: "Temperature (°C)" },
+  };
+
+  Plotly.newPlot("lineChart", [lineData], lineLayout);
+}
+
+// Function to display metadata
+function displayMetadata(metadata) {
+  const metadataDiv = document.getElementById("metadata");
+  metadataDiv.innerHTML = ""; // Clear previous metadata
+
+  for (const key in metadata) {
+      const p = document.createElement("p");
+      p.textContent = `${key}: ${metadata[key]}`;
+      metadataDiv.appendChild(p);
+  }
+}
+
+// Function to handle dropdown change
+function onCityChange() {
+  const selectedCity = document.getElementById("cityDropdown").value;
+  const selectedCityData = cityData[selectedCity];
+
+  createPrecipitationBarChart(selectedCityData);
+  createTemperatureLineChart(selectedCityData);
+  displayMetadata({ city: selectedCity }); // Display city metadata (you can add more metadata here)
+}
+
+// Event listener for dropdown change
+document.getElementById("cityDropdown").addEventListener("change", onCityChange);
+
+// Initialize the dashboard with the first city (e.g., New York City)
+onCityChange();
+
+
+
+
+/*
 // Define an object to store weather data
 const weatherData = {
   NewYorkCity: {
